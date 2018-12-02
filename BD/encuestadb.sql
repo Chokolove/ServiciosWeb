31b62b7c7360f6380d90b2d7dcde4f3244926575
Beyond Studios <beyond@beyondstudios.pe>
2018-12-02 16:54:49 -0500

    db update

--- BD/encuestadb.sql
+++ BD/encuestadb.sql
@@ -1,26 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.8.3
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 29-11-2018 a las 20:08:12
--- Versión del servidor: 10.1.36-MariaDB
--- Versión de PHP: 7.2.11
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `encuestadb`
---
 create database `encuestadb`;
 use `encuestadb`;
 
@@ -34,7 +12,7 @@ CREATE TABLE `empresa` (
   `idEmpresa` int(11) NOT NULL,
   `nomEmpresa` varchar(40) NOT NULL,
   `idTipoEmpresa` int(11) NOT NULL,
-  `fchaCreacion` varchar(10) NOT NULL,
+  `fechaCreacion` varchar(10) NOT NULL,
   `idEstado` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -42,11 +20,67 @@ CREATE TABLE `empresa` (
 -- Volcado de datos para la tabla `empresa`
 --
 
-INSERT INTO `empresa` (`idEmpresa`, `nomEmpresa`, `idTipoEmpresa`, `fchaCreacion`, `idEstado`) VALUES
+INSERT INTO `empresa` (`idEmpresa`, `nomEmpresa`, `idTipoEmpresa`, `fechaCreacion`, `idEstado`) VALUES
 (1, 'Developer', 1, '2018-11-17', 2),
 (2, 'Prueba1', 2, '2018-11-17', 2);
 
 -- --------------------------------------------------------
+
+
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipousuario`
+--
+
+CREATE TABLE `tipousuario` (
+  `idTipoUsuario` int(11) NOT NULL,
+  `nomTipoUsuario` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `tipousuario`
+--
+
+INSERT INTO `tipousuario` (`idTipoUsuario`, `nomTipoUsuario`) VALUES
+(1, 'Developer'),
+(2, 'Grunt'),
+(3, 'Administrador');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `idUsuario` int(11) NOT NULL,
+  `Login` varchar(40) NOT NULL,
+  `Pass` varchar(40) NOT NULL,
+  `nomUsu` varchar(255) NOT NULL,
+  `fechaCreacion` datetime NOT NULL default now(),
+  `idTipoUsuario` int(11) NOT NULL references tipousuario,
+  `idEstado` int(11) NOT NULL references tipoestado,
+  token varchar(100),
+  app_secret varchar(100)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`idUsuario`, `Login`, `Pass`, `nomUsu`, `fechaCreacion`, `idTipoUsuario`, `idEstado`) VALUES
+(1, 'Dev1', 'facil123', 'CarlosLL', '2018-11-17', 1, 2);
+
+
+
+CREATE TABLE `usuario_empresa` (
+  idUsuarioEmpresa int(11) not null primary key auto_increment,
+    idEmpresa int(11) references empresa(idEmpresa),
+    idUsuario int(11) references usuario(idUsuario)
+);
+
 
 --
 -- Estructura de tabla para la tabla `encuesta`
@@ -54,21 +88,39 @@ INSERT INTO `empresa` (`idEmpresa`, `nomEmpresa`, `idTipoEmpresa`, `fchaCreacion
 
 CREATE TABLE `encuesta` (
   `idEncuesta` int(11) NOT NULL,
-  `idUsuario` int(11) NOT NULL,
+  `idUsuario` int(11) NOT NULL references usuario,
   `nomEncuesta` varchar(40) NOT NULL,
-  `contRespuestas` int(11) NOT NULL,
-  `fchaCreacion` varchar(10) NOT NULL,
-  `idEstado` int(11) NOT NULL
+  `fechaCreacion` datetime NOT NULL default now(),
+  `idEstado` int(11) NOT NULL references tipoestado
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `encuesta`
 --
 
-INSERT INTO `encuesta` (`idEncuesta`, `idUsuario`, `nomEncuesta`, `contRespuestas`, `fchaCreacion`, `idEstado`) VALUES
-(1, 1, 'PruebaEncuesta1', 0, '2018-11-17', 2),
-(2, 1, 'PruebaEncuesta2', 0, '2018-11-17', 2),
-(3, 1, 'PruebaEncuesta2', 0, '2018-11-17', 1);
+INSERT INTO `encuesta` (`idEncuesta`, `idUsuario`, `nomEncuesta`, `fechaCreacion`, `idEstado`) VALUES
+(1, 1, 'PruebaEncuesta1', '2018-11-17', 2),
+(2, 1, 'PruebaEncuesta2', '2018-11-17', 2),
+(3, 1, 'PruebaEncuesta2', '2018-11-17', 1);
+
+
+--
+-- Estructura de tabla para la tabla `tipopregunta`
+--
+
+CREATE TABLE `tipopregunta` (
+  `idTipoPregunta` int(11) NOT NULL,
+  `nomTipoPregunta` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `tipopregunta`
+--
+
+INSERT INTO `tipopregunta` (`idTipoPregunta`, `nomTipoPregunta`) VALUES
+(1, 'Abierta'),
+(2, 'Radio'),
+(3, 'CheckBox');
 
 -- --------------------------------------------------------
 
@@ -78,9 +130,9 @@ INSERT INTO `encuesta` (`idEncuesta`, `idUsuario`, `nomEncuesta`, `contRespuesta
 
 CREATE TABLE `pregunta` (
   `idPregunta` int(11) NOT NULL,
-  `idEncuesta` int(11) NOT NULL,
+  `idEncuesta` int(11) NOT NULL references encuesta,
   `descPregunta` varchar(255) NOT NULL,
-  `idTipoPregunta` int(11) NOT NULL
+  `idTipoPregunta` int(11) NOT NULL references tipopregunta
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -101,26 +153,69 @@ INSERT INTO `pregunta` (`idPregunta`, `idEncuesta`, `descPregunta`, `idTipoPregu
 
 CREATE TABLE `respuesta` (
   `idRespuesta` int(11) NOT NULL,
-  `idPregunta` int(11) NOT NULL,
-  `descRespuesta` varchar(255) NOT NULL,
-  `contRespuesta` int(11) NOT NULL
+  `idPregunta` int(11) NOT NULL references pregunta,
+  `descRespuesta` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `respuesta`
 --
 
-INSERT INTO `respuesta` (`idRespuesta`, `idPregunta`, `descRespuesta`, `contRespuesta`) VALUES
-(1, 1, 'Respuesta1 Pregunta1', 0),
-(2, 1, 'Respuesta2 Pregunta1', 0),
-(3, 1, 'Respuesta3 Pregunta1', 0),
-(4, 2, 'Respuesta1 Pregunta2', 0),
-(5, 2, 'Respuesta2 Pregunta2', 0),
-(6, 2, 'Respuesta3 Pregunta2', 0),
-(7, 4, 'Respuesta1 Pregunta4', 0),
-(8, 4, 'Respuesta2 Pregunta4', 0),
-(9, 4, 'Respuesta3 Pregunta4', 0),
-(10, 3, '--Abierta--', 0);
+INSERT INTO `respuesta` (`idRespuesta`, `idPregunta`, `descRespuesta`) VALUES
+(1, 1, 'Respuesta1 Pregunta1'),
+(2, 1, 'Respuesta2 Pregunta1'),
+(3, 1, 'Respuesta3 Pregunta1'), 
+(10, 3, '--Abierta--');
+
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente_respuesta`
+--
+
+CREATE TABLE `cliente_encuesta` (
+  `idClienteEncuesta` int(11) NOT NULL,
+  `idEncuesta` int(11) NOT NULL references encuesta,
+  `userAgent` varchar(255),
+  `fechaCreacion` datetime NOT NULL default now()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente_respuesta`
+--
+
+CREATE TABLE `cliente_respuesta` (
+  `idClienteRespuesta` int(11) NOT NULL,
+  `idClienteEncuesta` int(11) NOT NULL references cliente_encuesta,
+  `idPregunta` int(11) NOT NULL references pregunta,
+  `string_value` varchar(255),
+  `string_value` varchar(255),
+  `int_value` int,
+  `double_value` double,
+  `fechaCreacion` datetime NOT NULL default now()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente_lista_respuesta`
+--
+
+CREATE TABLE `cliente_lista_respuesta` (
+  `idClienteListaRespuesta` int(11) NOT NULL,
+  `idClienteEncuesta` int(11) NOT NULL references cliente_encuesta,
+  `idPregunta` int(11) NOT NULL references pregunta,
+  `idRespuesta` int(11) NOT NULL references respuesta,
+  `fechaCreacion` datetime NOT NULL default now()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
 
 -- --------------------------------------------------------
 
@@ -162,234 +257,4 @@ INSERT INTO `tipoestado` (`idEstado`, `nomEstado`) VALUES
 
 -- --------------------------------------------------------
 
---
--- Estructura de tabla para la tabla `tipopregunta`
---
 
-CREATE TABLE `tipopregunta` (
-  `idTipoPregunta` int(11) NOT NULL,
-  `nomTipoPregunta` varchar(40) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Volcado de datos para la tabla `tipopregunta`
---
-
-INSERT INTO `tipopregunta` (`idTipoPregunta`, `nomTipoPregunta`) VALUES
-(1, 'Abierta'),
-(2, 'Radial'),
-(3, 'CheckBox');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `tipousuario`
---
-
-CREATE TABLE `tipousuario` (
-  `idTipoUsuario` int(11) NOT NULL,
-  `nomTipoUsuario` varchar(40) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Volcado de datos para la tabla `tipousuario`
---
-
-INSERT INTO `tipousuario` (`idTipoUsuario`, `nomTipoUsuario`) VALUES
-(1, 'Developer'),
-(2, 'Grunt'),
-(3, 'Administrador');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `usuario`
---
-
-CREATE TABLE `usuario` (
-  `idUsuario` int(11) NOT NULL,
-  `Login` varchar(40) NOT NULL,
-  `Pass` varchar(40) NOT NULL,
-  `nomUsu` varchar(255) NOT NULL,
-  `idEmpresa` int(11) NOT NULL,
-  `fchaCreacion` varchar(10) NOT NULL,
-  `idTipoUsuario` int(11) NOT NULL,
-  `idEstado` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Volcado de datos para la tabla `usuario`
---
-
-INSERT INTO `usuario` (`idUsuario`, `Login`, `Pass`, `nomUsu`, `idEmpresa`, `fchaCreacion`, `idTipoUsuario`, `idEstado`) VALUES
-(1, 'Dev1', 'facil123', 'CarlosLL', 1, '2018-11-17', 1, 2);
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `empresa`
---
-ALTER TABLE `empresa`
-  ADD PRIMARY KEY (`idEmpresa`),
-  ADD KEY `idTipoEmpresa` (`idTipoEmpresa`),
-  ADD KEY `idEstado` (`idEstado`);
-
---
--- Indices de la tabla `encuesta`
---
-ALTER TABLE `encuesta`
-  ADD PRIMARY KEY (`idEncuesta`),
-  ADD KEY `idUsuario` (`idUsuario`),
-  ADD KEY `idEstado` (`idEstado`);
-
---
--- Indices de la tabla `pregunta`
---
-ALTER TABLE `pregunta`
-  ADD PRIMARY KEY (`idPregunta`),
-  ADD KEY `idEncuesta` (`idEncuesta`),
-  ADD KEY `idTipoPregunta` (`idTipoPregunta`);
-
---
--- Indices de la tabla `respuesta`
---
-ALTER TABLE `respuesta`
-  ADD PRIMARY KEY (`idRespuesta`),
-  ADD KEY `idPregunta` (`idPregunta`);
-
---
--- Indices de la tabla `tipoempresa`
---
-ALTER TABLE `tipoempresa`
-  ADD PRIMARY KEY (`idTipoEmpresa`);
-
---
--- Indices de la tabla `tipoestado`
---
-ALTER TABLE `tipoestado`
-  ADD PRIMARY KEY (`idEstado`);
-
---
--- Indices de la tabla `tipopregunta`
---
-ALTER TABLE `tipopregunta`
-  ADD PRIMARY KEY (`idTipoPregunta`);
-
---
--- Indices de la tabla `tipousuario`
---
-ALTER TABLE `tipousuario`
-  ADD PRIMARY KEY (`idTipoUsuario`);
-
---
--- Indices de la tabla `usuario`
---
-ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`idUsuario`),
-  ADD KEY `idEmpresa` (`idEmpresa`),
-  ADD KEY `idTipoUsuario` (`idTipoUsuario`),
-  ADD KEY `idEstado` (`idEstado`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `empresa`
---
-ALTER TABLE `empresa`
-  MODIFY `idEmpresa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT de la tabla `encuesta`
---
-ALTER TABLE `encuesta`
-  MODIFY `idEncuesta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT de la tabla `pregunta`
---
-ALTER TABLE `pregunta`
-  MODIFY `idPregunta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT de la tabla `respuesta`
---
-ALTER TABLE `respuesta`
-  MODIFY `idRespuesta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-
---
--- AUTO_INCREMENT de la tabla `tipoempresa`
---
-ALTER TABLE `tipoempresa`
-  MODIFY `idTipoEmpresa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT de la tabla `tipoestado`
---
-ALTER TABLE `tipoestado`
-  MODIFY `idEstado` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT de la tabla `tipopregunta`
---
-ALTER TABLE `tipopregunta`
-  MODIFY `idTipoPregunta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT de la tabla `tipousuario`
---
-ALTER TABLE `tipousuario`
-  MODIFY `idTipoUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT de la tabla `usuario`
---
-ALTER TABLE `usuario`
-  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `empresa`
---
-ALTER TABLE `empresa`
-  ADD CONSTRAINT `empresa_ibfk_1` FOREIGN KEY (`idTipoEmpresa`) REFERENCES `tipoempresa` (`idTipoEmpresa`),
-  ADD CONSTRAINT `empresa_ibfk_2` FOREIGN KEY (`idEstado`) REFERENCES `tipoestado` (`idEstado`);
-
---
--- Filtros para la tabla `encuesta`
---
-ALTER TABLE `encuesta`
-  ADD CONSTRAINT `encuesta_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`),
-  ADD CONSTRAINT `encuesta_ibfk_2` FOREIGN KEY (`idEstado`) REFERENCES `tipoestado` (`idEstado`);
-
---
--- Filtros para la tabla `pregunta`
---
-ALTER TABLE `pregunta`
-  ADD CONSTRAINT `pregunta_ibfk_1` FOREIGN KEY (`idEncuesta`) REFERENCES `encuesta` (`idEncuesta`),
-  ADD CONSTRAINT `pregunta_ibfk_2` FOREIGN KEY (`idTipoPregunta`) REFERENCES `tipopregunta` (`idTipoPregunta`);
-
---
--- Filtros para la tabla `respuesta`
---
-ALTER TABLE `respuesta`
-  ADD CONSTRAINT `respuesta_ibfk_1` FOREIGN KEY (`idPregunta`) REFERENCES `pregunta` (`idPregunta`);
-
---
--- Filtros para la tabla `usuario`
---
-ALTER TABLE `usuario`
-  ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`idEmpresa`) REFERENCES `empresa` (`idEmpresa`),
-  ADD CONSTRAINT `usuario_ibfk_2` FOREIGN KEY (`idTipoUsuario`) REFERENCES `tipousuario` (`idTipoUsuario`),
-  ADD CONSTRAINT `usuario_ibfk_3` FOREIGN KEY (`idEstado`) REFERENCES `tipoestado` (`idEstado`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
